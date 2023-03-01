87293eabca66a1dfb4783cf5a4cc472be28c6abe
osppro <39586345+osppro@users.noreply.github.com>
2023-03-01 13:16:56 +0300

    code

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <div className="navbar">
+            <h1>Navbar</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">about</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/layouts/main.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar/navbar";
+function Main() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="content">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Main;
