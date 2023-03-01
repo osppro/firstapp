ea7452259d98b3925794766247366861a3a2bf63
osppro <39586345+osppro@users.noreply.github.com>
2023-03-01 14:38:22 +0300

    code

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,14 @@
+import  { NavLink } from "react-router-dom";
+
 function Navbar() {
     return (
         <div className="navbar">
             <h1>Navbar</h1>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">about</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/message">Messages</a></li>
+                <li><NavLink exact to="/">Home</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/message">Message</NavLink></li>
             </ul>
         </div>
     )
--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,11 +1,26 @@
+// import  { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
+import  { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
+
 import Navbar from "../components/navbar/navbar";
+import Home from "../components/home/home";
+import About from "../components/about/about";
+import Contact from "../components/contact/contact";
+import Message from "../components/message/message";
+
 function Main() {
     return (
         <div>
+        <HashRouter>
             <Navbar></Navbar>
             <div className="content">
-                
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/about" component={About}/>
+                <Route path="/message" component={Message}/>
+            </Switch>
             </div>
+        </HashRouter>
         </div>
     )
 }
